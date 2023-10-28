ce3a143766ee1697ef6600cdf5ae39d887531521
mudhirajkumar <mudhirajkumar78935@gmail.com>
2023-10-28 20:59:28 +0530

    1st commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+ region = "us-east-1"
+ access_key = "AKIAQ4KGIY56VQISWVXK"
+ secret_key = "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"
+}
+resource "aws_s3_instance" "terraform" {
+ bucket = "my-tf1235-bucket"
+ tags = {
+        Name = "my12terraform"
+        Environment = "dev"
+}
+}
+~      
