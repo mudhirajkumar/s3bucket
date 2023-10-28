5d99af5ebba570c486168f47d0b9fb0a1833019e
mudhirajkumar <141899019+mudhirajkumar@users.noreply.github.com>
2023-10-28 21:27:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
  access_key = "AKIAQ4KGIY56VQISWVXK"
  secret_key = "XNhOuEK4bJBtNida1igSB9v66/Tc3+nsAGJvsNVH"
 }
-resource "aws_s3_instance" "terraform" {
+resource "aws_s3_bucket" "example" {
  bucket = "my-tf1235-bucket"
  tags = {
         Name = "my12terraform"
